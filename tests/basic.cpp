fc8de90f63f9ef2d9efa59cdef88a67f0b5f1ffe
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2018-12-07 18:03:07 +0100

    tests/basic: write out png file

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -8,7 +8,13 @@
 
 #include <stillleben/phong_pass.h>
 
+#include <Magnum/Trade/AbstractImageConverter.h>
+#include <Magnum/Image.h>
+
 #include <iostream>
+
+using namespace Corrade;
+using namespace Magnum;
 
 // Could be so much nicer with concepts...
 template<std::size_t size, class T>
@@ -21,21 +27,36 @@ std::ostream& operator<<(std::ostream& stream, const Magnum::Math::Vector<size, 
 
 TEST_CASE("basic")
 {
+    // Create our stillleben Context
     auto context = sl::Context::Create();
     REQUIRE(context);
 
+    // Load a mesh file
     auto mesh = std::make_shared<sl::Mesh>(context);
     mesh->load(PATH_TO_SOURCES "/tests/stanford_bunny/scene.gltf");
 
+    // Create a scene
     sl::Scene scene(context, sl::ViewportSize(640, 480));
 
+    // Instantiate the mesh to create a movable scene object
     auto object = sl::Object::instantiate(mesh);
     REQUIRE(object);
 
+    // Render everything using a Phong shader
     sl::PhongPass phong;
     auto buffer = phong.render(scene);
 
     REQUIRE(buffer);
 
     CHECK(buffer->imageSize() == Magnum::Vector2i(640, 480));
+
+    {
+        PluginManager::Manager<Trade::AbstractImageConverter> manager;
+        std::unique_ptr<Trade::AbstractImageConverter> converter =
+            manager.loadAndInstantiate("PngImageConverter");
+        if(!converter) Fatal{} << "Cannot load the PngImageConverter plugin";
+
+        Image2D image = buffer->image({PixelFormat::RGBA8Unorm});
+        converter->exportToFile(image, "/tmp/stillleben.png");
+    }
 }
