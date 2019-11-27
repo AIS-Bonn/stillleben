4259a033480b88d35a66fd0d50e2c10aaefe9274
Max Schwarz <max.schwarz@online.de>
2019-11-27 01:09:36 +0100

    tangents: doc

--- include/stillleben/mesh_tools/tangents.h
+++ include/stillleben/mesh_tools/tangents.h
@@ -14,6 +14,14 @@
 namespace sl
 {
 
+/**
+ * @brief Extract tangents from TinyGltfImporter
+ *
+ * If you use Magnum::Trade::TinyGltfImporter, you can use this function to
+ * extract a tangent buffer for a given mesh.
+ *
+ * @warning Calling this when not using TinyGltfImporter will crash.
+ **/
 Corrade::Containers::Optional<std::vector<Magnum::Vector3>> extractTangents(
     const Magnum::Trade::AbstractImporter& importer,
     const Magnum::Trade::MeshData3D& mesh
