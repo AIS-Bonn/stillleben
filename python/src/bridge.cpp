de5acb4c6acbe08db5e4304d708144dabb05e2ba
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2020-04-15 14:41:02 +0200

    python: clean up main bridge file

--- python/src/bridge.cpp
+++ python/src/bridge.cpp
@@ -1,37 +1,7 @@
 // Python/C++ bridge
 // Author: Max Schwarz <max.schwarz@ais.uni-bonn.de>
 
-#include <torch/extension.h>
-
-#include <stillleben/context.h>
-#include <stillleben/mesh.h>
-#include <stillleben/object.h>
-#include <stillleben/scene.h>
-#include <stillleben/render_pass.h>
-#include <stillleben/debug.h>
-#include <stillleben/image_loader.h>
-#include <stillleben/cuda_interop.h>
-#include <stillleben/animator.h>
-#include <stillleben/light_map.h>
-#include <stillleben/mesh_cache.h>
-#include <stillleben/contrib/ctpl_stl.h>
-
-#include <Corrade/Utility/Configuration.h>
-#include <Corrade/Utility/Debug.h>
-#include <Corrade/Containers/ArrayView.h>
-
-#include <Magnum/Image.h>
-#include <Magnum/ImageView.h>
-#include <Magnum/PixelFormat.h>
-#include <Magnum/GL/TextureFormat.h>
-#include <Magnum/Magnum.h>
-
-#include <future>
-#include <memory>
-#include <functional>
-
-#include <pybind11/stl.h>
-#include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
 
 #include "py_context.h"
 #include "py_magnum.h"
